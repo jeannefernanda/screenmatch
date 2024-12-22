2b6b85bb97729c42097fd6763b78c252e38c716f
Jeanne <jeannefernanda.m@gmail.com>
2024-12-22 17:59:03 -0400

    Expressões lambdas

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -33,11 +33,13 @@ public class Principal {
 
         temporadas.forEach(System.out::println);
 
-        for(int i = 0; i < dados.totalTemporadas(); i++){
+/*        for(int i = 0; i < dados.totalTemporadas(); i++){
             List<DadosEpisodios> episodiosTemporada = temporadas.get(i).episodios();
             for(int j = 0; j < episodiosTemporada.size(); j++){
                 System.out.println(episodiosTemporada.get(j).titulo());
             }
-        }
+        }*/
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
